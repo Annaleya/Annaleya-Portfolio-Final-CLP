9f18662802cca382df89c3b793525c9244422bf6
Annaleya Hamilton <annaleyahamilton@outlook.com>
2021-01-24 18:53:21 -0500

    update js file and try to find issue with js element - still unsure

--- scripts.js
+++ scripts.js
@@ -8,9 +8,15 @@ var _CONTENT = [
 
 //Sentence being processed
 
+var _PART = 0;
+
+//character of sentence being processed
+
 var _PART_INDEX = 0;
 
-var _INTERVAL;
+//hold the value returned from setInterval
+
+var _INTERVAL_VAL;
 
 //Element that holds the text
 var _ELEMENT = document.querySelector("#text");
@@ -20,15 +26,15 @@ var _CURSOR = document.querySelector("#cursor");
 
 //typing effect
 function Type() {
-    var text = _CONTENT[_PART].substring(0, _PART_INDEX +1);
+    var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
     _ELEMENT.innerHTML = text;
     _PART_INDEX++;
 
     if(text === _CONTENT[_PART]) {
         _CURSOR.style.display = 'none';
 
-        clearInterval(_INTERVAL);
-        setTimeout(function(){
+        clearInterval(_INTERVAL_VAL);
+        setTimeout(function() {
             _INTERVAL_VAL = setInterval(Delete, 50);
         }, 1000);
     }
@@ -42,7 +48,7 @@ function Delete() {
     _PART_INDEX--;
 
     if(text === '') {
-        clearInterval(_INTERVAL);
+        clearInterval(_INTERVAL_VAL);
 
         if(_PART == (_CONTENT.length - 1))
             _PART = 0;
@@ -51,12 +57,12 @@ function Delete() {
 
         _PART_INDEX = 0;
 
-        setTimeout(function(){
+        setTimeout(function() {
             _CURSOR.style.display = 'inline-block';
-            _INTERVAL = setInterval(Type, 100);
+            _INTERVAL_VAL = setInterval(Type, 100);
         }, 200)
     }
 }
 
 //Start typing effect on load
-_INTERVAL = setInterval(Type, 100);
+_INTERVAL_VAL = setInterval(Type, 100);
