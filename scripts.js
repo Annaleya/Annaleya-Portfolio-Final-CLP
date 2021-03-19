b72ab69a4b97fb9fc06feea330a66da4152578b6
Annaleya Hamilton <annaleyahamilton@outlook.com>
2021-03-19 14:24:22 -0400

    Remove whitespace and clean up code

--- scripts.js
+++ scripts.js
@@ -110,31 +110,30 @@ const slidesCount = slides.childElementCount;
 const maxLeft = (slidesCount - 1) * 100 * -1;
 
 let current = 0;
-left = 0;
 
 function changeSlide(next = true) {
-  if (next) {
-    current += current > maxLeft ? -100 : current * -1;
-  } else {
-    current = current < 0 ? current + 100 : maxLeft;
-  }
+if (next) {
+current += current > maxLeft ? -100 : current * -1;
+} else {
+current = current < 0 ? current + 100 : maxLeft;
+}
 
-  slides.style.left = current + "%";
+slides.style.left = current + "%";
 }
 
 let autoChange = setInterval(changeSlide, delay);
 const restart = function() {
-  clearInterval(autoChange);
-  autoChange = setInterval(changeSlide, delay);
+clearInterval(autoChange);
+autoChange = setInterval(changeSlide, delay);
 };
 
 // Controls
 document.querySelector(".next-slide").addEventListener("click", function() {
-  changeSlide(false);
-  restart();
+changeSlide(false);
+restart();
 });
 
 document.querySelector(".prev-slide").addEventListener("click", function() {
-  changeSlide(false);
-  restart();
+changeSlide(false);
+restart();
 });
